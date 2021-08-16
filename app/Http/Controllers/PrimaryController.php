d7ad1a27dcd50668ae85896265004269dd2c8ffa
EslamSaad <eslamabu3esa@gmail.com>
2021-08-16 18:24:11 +0200

    gfd

--- app/Http/Controllers/PrimaryController.php
+++ app/Http/Controllers/PrimaryController.php
@@ -63,7 +63,7 @@ class PrimaryController extends Controller
 
     function saveLog(Request $request){
         $getTable = new DBLog;
-        $getTable->message =utf8_encode($request->input('message'));
+        $getTable->message =$request->input('message');
         $getTable->level = $request->input('level');
         $getTable->INFO = $request->input('INFO');
         $getTable->extra = $request->input('extra');
@@ -76,7 +76,7 @@ class PrimaryController extends Controller
 
     function saveLogtest(Request $request){
         $getTable = new DBLogTest;
-        $getTable->message =utf8_encode($request->input('message'));
+        $getTable->message =$request->input('message');
         $getTable->level = $request->input('level');
         $getTable->INFO = $request->input('INFO');
         $getTable->extra = $request->input('extra');
