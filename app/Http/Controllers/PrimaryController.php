123ba72f80b91f499f1501d970785e67040f4bbf
EslamSaad <eslamabu3esa@gmail.com>
2021-08-16 18:31:28 +0200

    gfd

--- app/Http/Controllers/PrimaryController.php
+++ app/Http/Controllers/PrimaryController.php
@@ -8,30 +8,31 @@ use App\Models\PrimaryModels as Moloquent;
 use App\Models\Account;
 use Illuminate\Http\Request;
 use Faker\Factory as Faker;
-use Input, Redirect;
+use Input;
+use Redirect;
 
 class PrimaryController extends Controller
 {
-
-    function home(Request $request, $template="home"){
-        if($request->uh=="123" && $request->up=="321"){
-
+    public function home(Request $request, $template="home")
+    {
+        if ($request->uh=="123" && $request->up=="321") {
             $getAllData = Moloquent::all();
             return view($template, ['data_user' => $getAllData]);
-        }else{
+        } else {
             echo "WELCOME HOME ::)))";
         }
     }
-    function hometest(Request $request, $template="home"){
-        if($request->uh=="123" && $request->up=="321"){
-
+    public function hometest(Request $request, $template="home")
+    {
+        if ($request->uh=="123" && $request->up=="321") {
             $getAllData = Moloquent::all();
             return view($template, ['data_user' => $getAllData]);
-        }else{
+        } else {
             echo "WELCOME HOME ::)))";
         }
     }
-    function index(Request $request){
+    public function index(Request $request)
+    {
         if ($request->un=="123" && $request->up=="321") {
             if ($request->start ==0) {
                 $request['page']=1;
@@ -49,19 +50,23 @@ class PrimaryController extends Controller
             $data['recordsTotal']=  $count;
             $data['recordsFiltered']=  $count;
             return response()->json($data);
-        }else{
+        } else {
             echo "WELCOME HOME 2 ::)))";
-
         }
-
     }
 
-    function pages($template){
-        $getAllData = Moloquent::all();
-        return view($template, ['data_user' => $getAllData]);
+    public function pages($template)
+    {
+        if ($request->un=="123" && $request->up=="321") {
+            $getAllData = Moloquent::all();
+            return view($template, ['data_user' => $getAllData]);
+        } else {
+            echo "WELCOME HOME 2 ::)))";
+        }
     }
 
-    function saveLog(Request $request){
+    public function saveLog(Request $request)
+    {
         $getTable = new DBLog;
         $getTable->message =$request->input('message');
         $getTable->level = $request->input('level');
@@ -74,7 +79,8 @@ class PrimaryController extends Controller
         return 1;
     }
 
-    function saveLogtest(Request $request){
+    public function saveLogtest(Request $request)
+    {
         $getTable = new DBLogTest;
         $getTable->message =$request->input('message');
         $getTable->level = $request->input('level');
@@ -86,7 +92,8 @@ class PrimaryController extends Controller
         return  $request;
     }
 
-    function save(Request $request){
+    public function save(Request $request)
+    {
         $getTable = new Moloquent;
         $getTable->username = $request->input('username');
         $getTable->email = $request->input('email');
@@ -94,10 +101,11 @@ class PrimaryController extends Controller
         $getTable->save();
 
         return Redirect::to('home')
-                ->with('success','You have been successfully insert data');
+                ->with('success', 'You have been successfully insert data');
     }
 
-    function update(Request $request){
+    public function update(Request $request)
+    {
         $id = $request->input('id');
         
         $getTable = Moloquent::find($id);
@@ -107,11 +115,11 @@ class PrimaryController extends Controller
         $getTable->save();
         
         return Redirect::to('home')
-                ->with('success','You have been successfully update data');
-
+                ->with('success', 'You have been successfully update data');
     }
 
-    function getdata($id){
+    public function getdata($id)
+    {
         $getData = Moloquent::where('_id', $id)
                             ->get();
         
@@ -119,37 +127,35 @@ class PrimaryController extends Controller
         
         return view('edit', ['get_user' => $getData, 'data_user' => $getAllData]);
     }
-    function delete($id){
-        
+    public function delete($id)
+    {
         $return = Moloquent::where('_id', $id)
                                 ->delete();
         
         return Redirect::to('home')
-                ->with('success','You have been successfully deleted data');
-
+                ->with('success', 'You have been successfully deleted data');
     }
-    function deleteAll(){
-        
-        $return = Moloquent::where('_id','!=','')->delete();
+    public function deleteAll()
+    {
+        $return = Moloquent::where('_id', '!=', '')->delete();
         
         return Redirect::to('home');
-
     }
     
-    function generateFake(Request $request){
+    public function generateFake(Request $request)
+    {
         $count = $request->input('fakedata');
 
-        if($count < 1000000){
-        	$faker = Faker::create();
-    		for ($i=1; $i <= $count; $i++) {
-    			$user = new Moloquent;
-    			$user->username = $faker->name;
-    			$user->email = $faker->email;
+        if ($count < 1000000) {
+            $faker = Faker::create();
+            for ($i=1; $i <= $count; $i++) {
+                $user = new Moloquent;
+                $user->username = $faker->name;
+                $user->email = $faker->email;
                 $user->address = $faker->address;
-    			$user->save();
-    		}
+                $user->save();
+            }
         }
         return Redirect::to('home');
     }
-    
 }
