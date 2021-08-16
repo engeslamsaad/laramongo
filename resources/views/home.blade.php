fb6af8d846c54e79cab905e28f1d3486d2484f09
EslamSaad <eslamabu3esa@gmail.com>
2021-08-16 17:13:01 +0200

    fgfdg

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,10 @@
 @extends('default')
 @section('content')
+<style>
+    table thead{
+        white-space:nowrap
+        }
+</style>
 <div class="row">
     {{-- <div class="col-md-4 col-sm-4 col-xs-4">
        <div class="panel panel-default">
